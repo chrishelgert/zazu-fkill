e84d07fe8b3203ffed17ebe044724057587b0475
Chris Helgert <chris.helgert@online.de>
2017-03-21 17:07:07 +0100

    add id´s for better internal-ranking

--- src/processes.js
+++ src/processes.js
@@ -4,18 +4,17 @@ module.exports.find = (input = '') => {
   const force = input.match(/\s(-f|--force)$/) !== null
   const cleanedInput = input.replace(' -f', '').replace(' --force', '')
 
-  return pslist()
-    .then(processes =>
-      processes
-        .filter(process => process.name.toLowerCase().indexOf(cleanedInput.toLowerCase()) !== -1)
-        .map(process => ({
-          title: process.name,
-          subtitle: process.cmd,
-          value: {
-            id: process.pid,
-            name: process.name,
-            force,
-          },
-        }))
-    )
+  return pslist().then(processes =>
+    processes
+      .filter(process => process.name.toLowerCase().indexOf(cleanedInput.toLowerCase()) !== -1)
+      .map(process => ({
+        id: process.name,
+        title: process.name,
+        subtitle: process.cmd,
+        value: {
+          id: process.pid,
+          name: process.name,
+          force,
+        },
+      })))
 }
